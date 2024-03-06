b981910d612659b103847183f0c87ad7337927cf
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-03-06 20:23:23 +0500

    Add files via upload

---
+++ Q12.ts
@@ -0,0 +1,4 @@
+//printing each person’s name, print a message to them.
+let friendsName = ["Sana", "Sara","Shazia","Samreen"];
+//The text of each message should be the same with personalize meassage
+friendsName.forEach(friendname =>console.log(`Hello ${friendname},Meet me at 5pm sharp`) );
